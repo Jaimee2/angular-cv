419e456f0bffb11e263c364fcb551981b70db142
Jaime <jaimehiguerasmedina98@gmail.com>
2023-09-18 10:09:56 +0200

    try template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CvComponentComponent } from './timeline-component/cv-component.component';
+import { PresentationLetterComponent } from './presentation-letter/presentation-letter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponentComponent,
+    PresentationLetterComponent
   ],
   imports: [
     BrowserModule
